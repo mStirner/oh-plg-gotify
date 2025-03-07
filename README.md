fefe95927f190f16fefbb794a0d430b3e2782a8f
Marc Stirner <marc.stirner@hotmail.de>
2025-03-07 12:36:19 +0100

    init

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Introduction
-This is a boilerplate plugin.
+Forwards notifications to a Gotify server
+
+> [!IMPORTANT]  
+> Dont forget to change the device IP Adress/Host to your gotify server!<br />
+> Dont forget to set the App secret in the "Gotify Server" vault!
 
 # Installation
 1) Create a new plugin over the OpenHaus backend HTTP API
@@ -11,23 +15,18 @@ Add plugin item via HTTP API:<br />
 [PUT] `http://{{HOST}}:{{PORT}}/api/plugins/`
 ```json
 {
-   "name":"Plugin Boilerplate",
+   "name":"Gotify Integration",
    "version": "1.0.0",
    "intents":[
       "devices",
-      "endpoints",
-      "plugins",
-      "rooms",
-      "ssdp",
       "store",
-      "users",
       "vault"
    ],
-   "uuid": "00000000-0000-0000-0000-000000000000"
+   "uuid": "65e83c09-82c5-49ac-822d-ffc164b9cea2"
 }
-
 ```
+
 Mount the source code into the backend plugins folder
 ```sh
-sudo mount --bind ~/projects/OpenHaus/plugins/plugin-boilerplate/ ~/projects/OpenHaus/backend/plugins/00000000-0000-0000-0000-000000000000/
+sudo mount --bind ~/projects/OpenHaus/plugins/oh-plg-gotify/ ~/projects/OpenHaus/backend/plugins/65e83c09-82c5-49ac-822d-ffc164b9cea2/
 ```
--- index.js
+++ index.js
@@ -1,41 +1,132 @@
+const { events } = require("../../system/notifications");
+const request = require("../../helper/request.js");
+
 module.exports = (info, logger, init) => {
     return init([
         "devices",
-        "endpoints",
-        "plugins",
-        "rooms",
-        "ssdp",
         "store",
-        "users",
         "vault"
-    ], (scope, [
+    ], async (scope, [
         C_DEVICES,
-        C_ENDPOINTS,
-        C_PLUGINS,
-        C_ROOMS,
-        C_SSDP,
         C_STORE,
-        C_USERS,
         C_VAULT
     ]) => {
 
 
-        // do something here with the components
-        // documentation about them can be found on:
-        // https://docs.open-haus.io/#/backend/components/
+        let device = await new Promise((resovle) => {
+            C_DEVICES.found({
+                labels: [
+                    "gotify=true"
+                ],
+            }, (device) => {
+
+                logger.debug(`Gotify server device found`, device);
+                resovle(device);
+
+            }, async (filter) => {
+
+                // add device
+                C_DEVICES.add({
+                    name: "Gotify Server",
+                    icon: "fa-solid fa-envelope",
+                    interfaces: [{
+                        settings: {
+                            host: "127.5.5.1",
+                            port: 80
+                        }
+                    }],
+                    ...filter
+                });
+
+            });
+        });
 
 
-        // scope = plugin class instace
-        console.log(scope);
+        let vault = await new Promise((resolve) => {
+            C_VAULT.found({
+                labels: [
+                    `device=${device._id}`,
+                    "gotify=true"
+                ]
+            }, (vault) => {
+
+                logger.debug(`Gotify vault found`, vault);
+                resolve(vault);
+
+            }, (filter) => {
+
+                C_VAULT.add({
+                    name: "Gotify Server",
+                    identifier: device._id,
+                    secrets: [{
+                        name: "Token",
+                        description: "App Token to be used as Authentication/App selection",
+                        key: "token"
+                    }],
+                    ...filter
+                });
+
+            });
+        });
 
 
-        // example logger usage
-        logger.trace("Hello World from plugin ", info.name);
-        logger.verbose("Hello World from plugin ", info.name);
-        logger.debug("Hello World from plugin ", info.name);
-        logger.info("Hello World from plugin ", info.name);
-        logger.warn("Hello World from plugin ", info.name);
-        logger.error("Hello World from plugin ", info.name);
+        Promise.all([device, vault]).then(() => {
+
+            let iface = device.interfaces[0];
+            let { host, port } = iface.settings;
+            let agent = iface.httpAgent();
+            let secret = vault.secrets[0];
+
+            events.on("publish", ({ title, message, type }) => {
+                try {
+
+                    logger.verbose("Event received", {
+                        title,
+                        message,
+                        type
+                    });
+
+                    request(`http://${host}:${port}/message?token=${secret.decrypt()}`, {
+                        agent,
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            title,
+                            message,
+                            priority: 5
+                        })
+                    }, (err, result) => {
+                        if (err || result?.body?.error) {
+
+                            // feedback
+                            logger.warn(err || result?.body?.error, "HTTP request failed");
+
+                        } else {
+
+                            let { id, date, priority } = result.body;
+
+                            // feedback
+                            logger.info(`Notification #${id} placed, date "${date}" as priority "${priority}"`);
+
+                        }
+                    });
+
+                } catch (err) {
+
+                    // feedacbk
+                    logger.error(err, "Could not send notification");
+
+                }
+            });
+
+        }).catch((err) => {
+
+            // feedback
+            logger.error(err, "Could not setup plugin");
+
+        });
 
 
     });
